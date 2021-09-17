7fbd05db4338443d73acd7b2ee005a1fc0e272c4
Alex Foster <A.Foster@Fourleaf.local>
2021-09-17 15:28:34 +0100

    Styles for demo

--- src/components/Chatbox/ChatboxCollapsed.js
+++ src/components/Chatbox/ChatboxCollapsed.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import ExpandCollapseToggle from "../ExpandCollapseToggle";
+import { Context } from "../Store";
 
-export default function ChatboxCollapsed(props) {
+export default function ChatboxCollapsed() {
+  const [state] = useContext(Context);
 
   return (
     <div
       fluid
       className="collapsed chatbox-wrapper"
+      style={{
+        position: state.domainID !== "CnDemo" ? "fixed" : "absolute",
+        bottom: state.domainID === "CnDem0" && 0,
+        right: state.domainID === "CnDem0" && 0,
+      }}
     >
       <ExpandCollapseToggle />
     </div>
