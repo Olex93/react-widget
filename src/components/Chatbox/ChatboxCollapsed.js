7bd54b6cbef6f9f5c5933e693cfe42b668e3b48a
Alex Foster <A.Foster@Fourleaf.local>
2021-09-20 09:46:24 +0100

    Entire new component

--- src/components/Chatbox/ChatboxCollapsed.js
+++ src/components/Chatbox/ChatboxCollapsed.js
@@ -1,16 +1,22 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import ExpandCollapseToggle from "../ExpandCollapseToggle";
 import { Context } from "../../Store";
 
 export default function ChatboxCollapsed(props) {
   const [state] = useContext(Context);
-  console.log('Preview mode from within collapsed: ' + props.previewMode)
+  console.log("Preview mode from within collapsed: " + props.previewMode);
   return (
-    <div
-      fluid
-      className={`collapsed chatbox-wrapper  ${props.previewMode === true ? 'pinnedToDiv' : ''}`}
-    >
-      <ExpandCollapseToggle />
-    </div>
+    <>
+      {props.previewMode === true && (
+        <div fluid className="collapsed chatbox-wrapper pinnedToDiv">
+          <ExpandCollapseToggle />
+        </div>
+      )}
+      {props.previewMode === false && (
+        <div fluid className="collapsed chatbox-wrapper">
+          <ExpandCollapseToggle />
+        </div>
+      )}
+    </>
   );
 }
