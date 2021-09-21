0b89c9f6b99ed02112ae23c47079e7a31b1958d6
Alex Foster <A.Foster@Fourleaf.local>
2021-09-21 08:55:27 +0100

    Saved sequencing

--- src/RootComponent.js
+++ src/RootComponent.js
@@ -139,7 +139,7 @@ export default function RootComponent(props) {
   };
 
   let resourceSizes = [];
-
+  
   useEffect(() => {
     // getLocalDomainId()
     const productKey = props.domElement.dataset["productkey"];
